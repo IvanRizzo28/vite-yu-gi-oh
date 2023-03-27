9d204bfe13f9737885b98ee5965a60b4bc3121ad
IvanRizzo28 <22ivanrizzo@gmail.com>
2023-03-27 17:21:04 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import './../node_modules/roboto-fontface/css/roboto/roboto-fontface.css';
+import App from './App.vue'
+
+createApp(App).mount('#app')
